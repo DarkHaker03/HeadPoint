54a69828782bf08580471179ee031effea10cb5a
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2023-07-18 14:46:12 +0500

    Add project files.

---
+++ HeadPoint/Program.cs
@@ -0,0 +1,23 @@
+﻿static IEnumerable<IGrouping<string, string>> ToEnumerableGroupString(IEnumerable<string> enumerableString)
+{
+    return enumerableString.GroupBy(s =>
+    {
+        var sortedString = s.ToLower().ToArray();
+        Array.Sort(sortedString);
+        return new string(sortedString);
+    });
+}
+
+// Проверка
+var wordArray = new string[] { "ток", "рост", "кот", "торс", "Кто", "фывап", "рок" };
+
+var wordGroups = ToEnumerableGroupString(wordArray);
+
+foreach (var group in wordGroups)
+{
+    foreach (var word in group)
+    {
+        Console.WriteLine(word);
+    }
+    Console.WriteLine("-----------------");
+}
